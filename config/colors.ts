89cd7de81e7d17e9bbce29f1aef23a04baf70949
Ulises <ulises.aviles@cetys.edu.mx>
2022-04-12 11:56:46 -0700

    Colors done

--- config/colors.ts
+++ config/colors.ts
@@ -1,14 +1,48 @@
 export default {
   dark: {
-    background: "rgb(12,20,29)",
+    background: "rgb(6,12,16)",
     font: {
       primary: "rgb(251, 251, 251)",
+      secondary: "rgb(180, 180, 180)",
+      accent: "rgb(45, 112, 243)",
+    },
+    input: {
+      background: "rgb(31, 38, 43)",
+      placeholder: "rgb(143, 148, 152)",
+    },
+    btn: {
+      background: "rgb(20, 35, 51)",
+    },
+    gradients: {
+      main: ["rgb(44, 99, 244)", "rgb(52, 185, 233)"],
+      red: ["rgb(255, 0, 0)", "rgb(208, 76, 116)"],
+    },
+    messages: {
+      sender: ["rgb(22, 148, 186)", "rgb(11, 61, 193)"],
+      receiver: "rgb(31, 38, 43)",
     },
   },
   light: {
     background: "rgb(255,255,255)",
     font: {
       primary: "rgb(0, 0, 0)",
+      secondary: "rgb(125, 125, 125)",
+      accent: "rgb(45, 112, 243)",
+    },
+    input: {
+      background: "rgb(241, 241, 241)",
+      placeholder: "rgb(150, 150, 150)",
+    },
+    btn: {
+      background: "rgb(12, 20, 29)",
+    },
+    gradients: {
+      main: ["rgb(44, 99, 244)", "rgb(52, 185, 233)"],
+      red: ["rgb(255, 0, 0)", "rgb(208, 76, 116)"],
+    },
+    messages: {
+      sender: ["rgb(22, 148, 186)", "rgb(11, 61, 193)"],
+      receiver: "rgb(241, 241, 241)",
     },
   },
 };
