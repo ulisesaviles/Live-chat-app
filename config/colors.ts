3e31706d5860bc6ec5d1c5855b1d500fc94fad8e
Ulises <ulises.aviles@cetys.edu.mx>
2022-04-11 21:38:51 -0700

    Added fontColors

--- config/colors.ts
+++ config/colors.ts
@@ -1,8 +1,14 @@
 export default {
   dark: {
     background: "rgb(12,20,29)",
+    font: {
+      primary: "rgb(251, 251, 251)",
+    },
   },
   light: {
     background: "rgb(255,255,255)",
+    font: {
+      primary: "rgb(0, 0, 0)",
+    },
   },
 };
