a97a8ab2eb36f8325e178ab4cff4659340d6db51
Ulises <ulises.aviles@cetys.edu.mx>
2022-04-14 23:15:07 -0700

    Added small prop

--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,11 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { Appearance, Dimensions, StyleSheet, TouchableOpacity, Text } from "react-native";
+import {
+  Appearance,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import colors from "../config/colors";
 
 // Constants
@@ -9,44 +15,43 @@ const dimensions = {
 };
 
 const getColorScheme = () => {
-  return Appearance.getColorScheme() || 'dark';
+  return Appearance.getColorScheme() || "dark";
 };
 
-const styles = StyleSheet.create({
-  text: {
-    color: "white",
-    fontSize: 22,
-    fontWeight: "500",
-  },
-  primaryBtn: {
-    paddingVertical: 8,
-    paddingHorizontal: 50,
-    borderRadius: 20,
-    backgroundColor: colors[getColorScheme()].btn.background,
-    marginTop: dimensions.height * 0.025,
-    marginBottom: 20,
-  },
-  gradientBtn: {
-    paddingVertical: 8,
-    paddingHorizontal: 50,
-    borderRadius: 20,
-  },
-})
-
 interface ButtonProps {
-  type: 'primary'| 'gradient';
+  type: "primary" | "gradient";
   value: string;
   onPress: any;
+  small?: boolean;
 }
 
 export const Button = (props: ButtonProps): JSX.Element => {
-  return (
-    props.type === 'primary'
-    ?
+  const styles = StyleSheet.create({
+    text: {
+      color: "white",
+      fontSize: !props.small ? 22 : 16,
+      fontWeight: "500",
+    },
+    primaryBtn: {
+      paddingVertical: !props.small ? 8 : 3,
+      paddingHorizontal: !props.small ? 50 : 15,
+      borderRadius: !props.small ? 20 : 11,
+      backgroundColor: colors[getColorScheme()].btn.background,
+      marginTop: dimensions.height * 0.025,
+      marginBottom: 20,
+    },
+    gradientBtn: {
+      paddingVertical: !props.small ? 8 : 3,
+      paddingHorizontal: !props.small ? 50 : 15,
+      borderRadius: !props.small ? 20 : 11,
+    },
+  });
+
+  return props.type === "primary" ? (
     <TouchableOpacity onPress={props.onPress} style={styles.primaryBtn}>
       <Text style={styles.text}>Log in</Text>
     </TouchableOpacity>
-    :
+  ) : (
     <TouchableOpacity onPress={props.onPress}>
       <LinearGradient
         colors={colors[getColorScheme()].gradients.main}
@@ -57,5 +62,5 @@ export const Button = (props: ButtonProps): JSX.Element => {
         <Text style={styles.text}>{props.value}</Text>
       </LinearGradient>
     </TouchableOpacity>
-  )
-}
+  );
+};
