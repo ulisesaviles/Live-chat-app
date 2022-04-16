d676c76488366a1ed14812b45c1f2a67b81028fc
Ulises <ulises.aviles@cetys.edu.mx>
2022-04-16 12:31:47 -0700

    Now, it detects when app changes colorScheme

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,14 @@
-import { FC, useState } from "react";
-import { 
+import { FC, useEffect, useState } from "react";
+import {
   Dimensions,
   StyleSheet,
   useColorScheme,
   View,
   Text,
   TextInput,
-  Appearance
-} from "react-native"
+  Appearance,
+} from "react-native";
 import colors from "../config/colors";
-import { ColorSchemeType } from "../types";
 
 // Constants
 const dimensions = {
@@ -17,34 +16,34 @@ const dimensions = {
   width: Dimensions.get("screen").width,
 };
 
-const getColorScheme = () => {
-  return Appearance.getColorScheme() || 'dark';
-};
-
-const styles = StyleSheet.create({
-  input: {
-    backgroundColor: colors[getColorScheme()].input.background,
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-    fontSize: 18,
-    borderRadius: 10,
-    width: "100%",
-    color: colors[getColorScheme()].font.primary,
-  },
-  inputContainer: {
-    width: dimensions.width * 0.9,
-    marginBottom: 30,
-  },
-  inputTitle: {
-    color: colors[getColorScheme()].font.secondary,
-    fontWeight: "600",
-    marginBottom: 10,
-    fontSize: 16,
-  },
-})
-
 interface InputProps {
-  type: 'none'| 'URL' | 'addressCity' | 'addressCityAndState' | 'addressState' | 'countryName' | 'creditCardNumber' | 'emailAddress' | 'familyName' | 'fullStreetAddress' | 'givenName' | 'jobTitle' | 'location' | 'middleName' | 'name' | 'namePrefix' | 'nameSuffix' | 'nickname' | 'organizationName' | 'postalCode' | 'streetAddressLine1' | 'streetAddressLine2' | 'sublocality' | 'telephoneNumber' | 'username' | 'password';
+  type:
+    | "none"
+    | "URL"
+    | "addressCity"
+    | "addressCityAndState"
+    | "addressState"
+    | "countryName"
+    | "creditCardNumber"
+    | "emailAddress"
+    | "familyName"
+    | "fullStreetAddress"
+    | "givenName"
+    | "jobTitle"
+    | "location"
+    | "middleName"
+    | "name"
+    | "namePrefix"
+    | "nameSuffix"
+    | "nickname"
+    | "organizationName"
+    | "postalCode"
+    | "streetAddressLine1"
+    | "streetAddressLine2"
+    | "sublocality"
+    | "telephoneNumber"
+    | "username"
+    | "password";
   label: string;
   value?: string;
   onChangeText: any;
@@ -53,8 +52,44 @@ interface InputProps {
 }
 
 const Input = (props: InputProps): JSX.Element => {
+  // State handlers
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+  const [firstLoad, setFirstLoad] = useState(true);
 
-  return(
+  // Styles
+  const styles = StyleSheet.create({
+    input: {
+      backgroundColor: colors[colorScheme!].input.background,
+      paddingVertical: 8,
+      paddingHorizontal: 10,
+      fontSize: 18,
+      borderRadius: 10,
+      width: "100%",
+      color: colors[colorScheme!].font.primary,
+    },
+    inputContainer: {
+      width: dimensions.width * 0.9,
+      marginBottom: 30,
+    },
+    inputTitle: {
+      color: colors[colorScheme!].font.secondary,
+      fontWeight: "600",
+      marginBottom: 10,
+      fontSize: 16,
+    },
+  });
+
+  // Colorscheme change handler
+  useEffect(() => {
+    if (firstLoad) {
+      setFirstLoad(false);
+      Appearance.addChangeListener(() => {
+        setColorScheme(Appearance.getColorScheme());
+      });
+    }
+  });
+
+  return (
     <View style={styles.inputContainer}>
       <Text style={styles.inputTitle}>{props.label}</Text>
       <TextInput
@@ -62,15 +97,13 @@ const Input = (props: InputProps): JSX.Element => {
         value={props.value}
         onChangeText={props.onChangeText}
         placeholder={props.placeholder}
-        placeholderTextColor={
-          colors[getColorScheme()].input.placeholder
-        }
+        placeholderTextColor={colors[colorScheme!].input.placeholder}
         textContentType={props.type}
         clearButtonMode="while-editing"
         secureTextEntry={props.secureTextEntry}
       />
     </View>
-    )
-}
+  );
+};
 
-export {Input}
+export { Input };
