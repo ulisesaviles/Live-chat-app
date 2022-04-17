795b078659762de92ea39a4e8889d5c6a202da04
David <david.dennis@cetys.edu.mx>
2022-04-17 12:48:59 -0700

    Created conversation for home

--- types/index.ts
+++ types/index.ts
@@ -1,21 +1,36 @@
+// import interfaces
+import { User } from "../interfaces";
+
 // This type allows to handle colors easyly
 export type ColorSchemeType = "dark" | "light";
 
 // Message type
 export type messageType = "img" | "txt";
 
-export enum InCallOptions {
-  VIDEO = 'video',
-  AUDIO = 'audio',
-  SWAP = 'swap',
-  END_CALL = 'endCall',
-  ANSWER_CALL = 'answer'
+// Conversation for home
+export type ConversationForHome = {
+  user: User;
+  conversationId: string;
+  lastMessage?: {
+    text?: string;
+    timestamp: number;
+    senderId?: string;
+    type: "txt" | "img";
+  };
+  chatId?: string;
 };
+
+export enum InCallOptions {
+  VIDEO = "video",
+  AUDIO = "audio",
+  SWAP = "swap",
+  END_CALL = "endCall",
+  ANSWER_CALL = "answer",
+}
 
 export enum CallStates {
-  WAITING = 'waiting',
-  ON_CALL = 'onCall',
-  CALL_ENDED = 'ended',
-  INCOMING_CALL = 'incoming'
-};
-
+  WAITING = "waiting",
+  ON_CALL = "onCall",
+  CALL_ENDED = "ended",
+  INCOMING_CALL = "incoming",
+}
